75bec1e96d8842b17f93b50cf34af439eb081ef5
Mahmoud Elgalawy <elgalawymahmoud99@gmail.com>
2024-08-21 12:30:42 +0300

    Initial Commit

---
+++ firstSwiftUI/firstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstSwiftUIApp.swift
+//  firstSwiftUI
+//
+//  Created by Mahmoud  on 21/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct firstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
